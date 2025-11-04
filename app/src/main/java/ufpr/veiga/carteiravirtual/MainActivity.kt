589c2787445dc863e37a042e0282fcb407f5131f
Vitor Adriel Deganelli Espinoza Moya <T3756460@timbrasil.com.br>
2025-11-04 20:07:12 -0300

    feat: adiciona ui

--- app/src/main/java/ufpr/veiga/carteiravirtual/MainActivity.kt
+++ app/src/main/java/ufpr/veiga/carteiravirtual/MainActivity.kt
@@ -1,20 +1,59 @@
 package ufpr.veiga.carteiravirtual
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.text.NumberFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private var saldoBRL: Double = 0.0
+    private var saldoUSD: Double = 0.0
+    private var saldoBTC: Double = 0.0
+
+    private lateinit var tvSaldoReais: TextView
+    private lateinit var tvSaldoDolares: TextView
+    private lateinit var tvSaldoBitcoin: TextView
+    private lateinit var btnIrParaConversao: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvSaldoReais = findViewById(R.id.tvSaldoReais)
+        tvSaldoDolares = findViewById(R.id.tvSaldoDolares)
+        tvSaldoBitcoin = findViewById(R.id.tvSaldoBitcoin)
+        btnIrParaConversao = findViewById(R.id.btnIrParaConversao)
+
+        atualizarSaldos()
+
+        btnIrParaConversao.setOnClickListener {
+            val intent = Intent(this, ConverterActivity::class.java)
+            intent.putExtra("saldoBRL", saldoBRL)
+            intent.putExtra("saldoUSD", saldoUSD)
+            intent.putExtra("saldoBTC", saldoBTC)
+            startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        saldoBRL = intent.getDoubleExtra("saldoBRL", saldoBRL)
+        saldoUSD = intent.getDoubleExtra("saldoUSD", saldoUSD)
+        saldoBTC = intent.getDoubleExtra("saldoBTC", saldoBTC)
+        atualizarSaldos()
+    }
+
+    private fun atualizarSaldos() {
+        val formatadorBRL = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+        tvSaldoReais.text = "Saldo BRL: ${formatadorBRL.format(saldoBRL)}"
+
+        val formatadorUSD = NumberFormat.getCurrencyInstance(Locale.US)
+        tvSaldoDolares.text = "Saldo USD: ${formatadorUSD.format(saldoUSD)}"
+
+        tvSaldoBitcoin.text = "Saldo BTC: ₿ %.8f".format(saldoBTC)
     }
 }
